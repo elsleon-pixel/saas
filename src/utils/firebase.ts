a85ade80e97bf82bf116769361fdd43156bb65c1
Leon Els <elsleon@gmail.com>
2025-12-30 20:11:44 -0700

    fix

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,21 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Fix: Suppress TS errors for Firebase imports as the modular structure is correct but types are missing in the environment.
-// @ts-ignore
-import { initializeApp, getApps } from "firebase/app";
-// @ts-ignore
-import { getAuth } from "firebase/auth";
-// @ts-ignore
-import { getFirestore } from "firebase/firestore";
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCi04Nhzpcl18wZO102U_vYaxeB4uC2CCY",
-  authDomain: "the-poker-tour.firebaseapp.com",
-  projectId: "the-poker-tour",
-  storageBucket: "the-poker-tour.firebasestorage.app",
-  messagingSenderId: "100992673768",
-  appId: "1:100992673768:web:5573d839222cfc33df265d"
+  apiKey: "AIzaSyDvaMzABRwl7Mk4kF9h2RqQV7Q-7x6m-oc",
+  authDomain: "chiptalk-poker-prod.firebaseapp.com",
+  projectId: "chiptalk-poker-prod",
+  storageBucket: "chiptalk-poker-prod.firebasestorage.app",
+  messagingSenderId: "666255988426",
+  appId: "1:666255988426:web:2a2384b4ff3b6be7ca0633",
+  measurementId: "G-TRG30GVFS4"
 };
 
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
